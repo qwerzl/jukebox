855d42d2d69666a758641c48e2ddc5b0c022ebef
qwerzl <me@qwerzl.me>
2024-11-24 03:06:16 +0000

    feat: support multiple user stats

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts">
 import { useIntervalFn } from '@vueuse/core'
-import Lottie from 'lottie-web'
+import Lottie, { type AnimationItem } from 'lottie-web'
 import { useTemplateRef } from 'vue'
+import { Vue3Lottie } from 'vue3-lottie'
 import { useMusicStore } from '~/store/music'
 
 enum UserStatus {
-  Idle,
   Walking,
+  Idle,
   Vehicle
 }
 
 const music = useMusicStore()
 const playbackTime = ref(0)
 const playbackDuration = ref(0)
+const userStatus = ref(UserStatus.Walking)
 const lottieContainer = useTemplateRef('lottieContainer')
 
 useIntervalFn(() => {
@@ -20,26 +22,57 @@ useIntervalFn(() => {
   playbackDuration.value = music.howlInstance?.duration() ?? 0
 }, 100)
 
-// watch(() => music.bpm, () => {
-//   lottieReloadKey.value++
-// })
+const lottieContainerStyle = ref()
 
-const animationData = await fetch('/walking.json').then(res => res.json())
+const lottieAnimation = ref<AnimationItem>()
 onMounted(() => {
-  console.log(animationData)
   if (!lottieContainer.value) return
-  const lottieAnimation = Lottie.loadAnimation({
-    container: lottieContainer.value,
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    animationData,
-  })
-  lottieAnimation.setSpeed(0.8 / (60 / music.bpm))
+
+  watch(() => userStatus.value, async () => {
+    lottieAnimation.value?.destroy()
+    if (!lottieContainer.value) return
+    const animationData = await (async () => {
+      let path = ''
+      switch (userStatus.value) {
+        case UserStatus.Walking:
+          path = '/walking.json'
+          break
+        case UserStatus.Idle:
+          path = '/sitting.json'
+          break
+      }
+      return await fetch(path)
+        .then(res => res.json())
+    })()
+    lottieAnimation.value = Lottie.loadAnimation({
+      container: lottieContainer.value,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData,
+    })
+    lottieContainerStyle.value = [
+      {
+        marginTop: '-150px',
+        marginRight: '-80px',
+        width: '300px',
+        height: '300px'
+      },
+      {
+        marginTop: '100px',
+        marginRight: '-80px',
+        width: '250px',
+        height: '250px',
+      }
+    ][userStatus.value as number]
+  }, { immediate: true })
 
   watch(() => music.bpm, () => {
-    lottieAnimation.setSpeed(0.8 / (60 / music.bpm))
-  })
+    switch (userStatus.value) {
+      case UserStatus.Walking:
+        lottieAnimation.value?.setSpeed(0.8 / (60 / music.bpm))
+    }
+  }, { immediate: true })
 })
 
 function secondsToTime(e: number) {
@@ -81,17 +114,33 @@ function secondsToTime(e: number) {
     </div>
     <div class="pb-10 border font-grotesk">
       <div class="grid grid-cols-2 w-full h-full gap-2">
-        <div class="w-full rounded aspect-square bg-[#FFFFFF20] grid">
-          <div class="w-full h-full flex items-center justify-center col-start-1 row-start-1 overflow-hidden">
-            <div ref="lottieContainer" style="margin-top: -150px; margin-right: -80px;" class="w-[300px] h-[300px]" />
-          </div>
-          <div class="h-full w-full col-start-1 row-start-1 flex justify-end flex-col px-2 py-0.5">
-            <div class="flex justify-between flex-col items-start h-full">
-              <div class="text-neutral-600">
-                {{ music.bpm }} BPM
+        <div class="w-full aspect-square overflow-hidden">
+          <div class="w-full rounded bg-[#FFFFFF20] grid transition-all ease-in-out duration-300 origin-bottom-left" :class="{'aspect-square': userStatus === UserStatus.Walking, 'aspect-[2/1] mb-1': userStatus === UserStatus.Idle}" @click="userStatus = userStatus === UserStatus.Walking ? UserStatus.Idle : UserStatus.Walking">
+            <div class="w-full h-full flex items-center justify-center col-start-1 row-start-1 overflow-hidden">
+              <div ref="lottieContainer" :style="lottieContainerStyle" class="w-[300px] h-[300px]" />
+            </div>
+            <div class="h-full w-full col-start-1 row-start-1 flex justify-end flex-col px-2 py-0.5">
+              <div class="flex justify-between flex-col-reverse items-start h-full">
+                <div class="text-black text-lg">
+                  {{ userStatus === UserStatus.Walking ? 'Walking' : 'Idle' }}
+                </div>
+                <div class="text-neutral-600 transition-all ease-in-out duration-300" :class="{'opacity-0': userStatus === UserStatus.Idle, 'opacity-100': userStatus === UserStatus.Walking}">
+                  {{ music.bpm }} BPM
+                </div>
               </div>
+            </div>
+          </div>
+          <div class="w-full pt-1 bg-[#FFFFFF20] transition-all ease-in-out duration-300 overflow-hidden aspect-[2/1] rounded grid" :class="{'h-0': userStatus === UserStatus.Walking, 'aspect-[1/2]': userStatus === UserStatus.Idle}">
+            <Vue3Lottie
+              animation-link="/house.json"
+              :height="150"
+              :width="150"
+              style="margin-top: 0px; margin-right: -50px;"
+              class="col-start-1 row-start-1"
+            />
+            <div class="col-start-1 row-start-1 w-full h-full flex flex-col justify-between px-2">
               <div class="text-black text-lg">
-                Walking
+                At home
               </div>
             </div>
           </div>
