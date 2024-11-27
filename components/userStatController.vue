8854b83116adb7e8673915d7db3af9f266ea20f0
qwerzl <me@qwerzl.me>
2024-11-27 10:30:14 +0800

    feat: add idle scenarios

--- components/userStatController.vue
+++ components/userStatController.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="w-full aspect-square overflow-hidden">
-    <div class="w-full rounded bg-[#b1b1b1] grid transition-all ease-in-out duration-300 origin-bottom-left z-30" :class="{'aspect-square': userStatus === UserStatus.Walking, 'aspect-[2/1] mb-1': userStatus === UserStatus.Idle}" @click="userStatus = userStatus === UserStatus.Walking ? UserStatus.Idle : UserStatus.Walking">
+    <div class="w-full rounded bg-[#b1b1b1] grid transition-all ease-in-out duration-300 origin-bottom-left z-30" :class="{'aspect-square': userStatus === UserStatus.Walking, 'aspect-[2/1] mb-1': userStatus === UserStatus.Idle}" @click="switchStatus()">
       <div class="w-full h-full flex items-center justify-center col-start-1 row-start-1 overflow-hidden z-30">
-        <div ref="lottieContainer" :style="lottieContainerStyle" class="w-[300px] h-[300px]" />
+        <div ref="lottieContainer" :style="lottieContainerStyle" />
       </div>
       <div class="h-full w-full col-start-1 row-start-1 flex justify-end flex-col px-2 py-0.5">
         <div class="flex justify-between flex-col-reverse items-start h-full">
@@ -16,16 +16,10 @@
       </div>
     </div>
     <div class="w-full pt-1 bg-[#b1b1b1] transition-all ease-in-out duration-300 overflow-hidden aspect-[2/1] rounded grid" :class="{'h-0': userStatus === UserStatus.Walking, 'aspect-[1/2]': userStatus === UserStatus.Idle}">
-      <Vue3Lottie
-        animation-link="/house.json"
-        :height="150"
-        :width="150"
-        style="margin-top: 0px; margin-right: -50px;"
-        class="col-start-1 row-start-1"
-      />
+      <div ref="idleLottieContainer" :style="idleLottieContainerStyle" class="col-start-1 row-start-1" />
       <div class="col-start-1 row-start-1 w-full h-full flex flex-col justify-between px-2">
         <div class="text-black text-lg">
-          At home
+          {{ idleLocation === IdleLocation.Home ? 'At home' : idleLocation === IdleLocation.Work ? 'At work' : 'In Starbucks' }}
         </div>
       </div>
     </div>
@@ -42,13 +36,48 @@ enum UserStatus {
   Vehicle
 }
 
+enum IdleLocation {
+  Home,
+  Work,
+  Starbucks
+}
+
 const music = useMusicStore()
 const userStatus = ref(UserStatus.Walking)
+const idleLocation = ref(IdleLocation.Home)
+
 const lottieContainer = useTemplateRef<HTMLElement>('lottieContainer')
+const idleLottieContainer = useTemplateRef<HTMLElement>('idleLottieContainer')
 
 const lottieContainerStyle = ref()
-
 const lottieAnimation = ref<AnimationItem>()
+
+const idleLottieContainerStyle = ref()
+const idleLottieAnimation = ref<AnimationItem>()
+
+
+function switchStatus() {
+  switch (userStatus.value) {
+    case UserStatus.Walking:
+      userStatus.value = UserStatus.Idle
+      break
+    case UserStatus.Idle:
+      switch (idleLocation.value) {
+        case IdleLocation.Home:
+          idleLocation.value = IdleLocation.Work
+          break
+        case IdleLocation.Work:
+          idleLocation.value = IdleLocation.Starbucks
+          break
+        case IdleLocation.Starbucks:
+          idleLocation.value = IdleLocation.Home
+          userStatus.value = UserStatus.Walking
+          break
+      }
+      break
+  }
+}
+
 onMounted(() => {
   if (!lottieContainer.value) return
 
@@ -93,6 +122,54 @@ onMounted(() => {
       case UserStatus.Walking:
         lottieAnimation.value?.setSpeed(0.8 / (60 / music.bpm))
     }
+  }, { immediate: true })
+
+  watch(() => idleLocation.value, async () => {
+    idleLottieAnimation.value?.destroy()
+    if (!idleLottieContainer.value) return
+    const animationData = await (async () => {
+      let path = ''
+      switch (idleLocation.value) {
+        case IdleLocation.Home:
+          path = '/house.json'
+          break
+        case IdleLocation.Work:
+          path = '/office.json'
+          break
+        case IdleLocation.Starbucks:
+          path = '/coffee.json'
+          break
+      }
+      return await fetch(path)
+        .then(res => res.json())
+    })()
+    idleLottieAnimation.value = Lottie.loadAnimation({
+      container: idleLottieContainer.value,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData,
+    })
+    idleLottieContainerStyle.value = [
+      {
+        marginTop: '0px',
+        marginLeft: '120px',
+        width: '150px',
+        height: '150px'
+      },
+      {
+        marginTop: '-150px',
+        marginLeft: '-50px',
+        width: '400px',
+        height: '400px',
+      },
+      {
+        marginTop: '-30px',
+        marginLeft: '100px',
+        width: '180px',
+        height: '180px',
+      }
+    ][idleLocation.value as number]
   }, { immediate: true })
 
   watch(() => music.bpm, () => {
